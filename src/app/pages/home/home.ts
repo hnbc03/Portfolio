d2ec6c819431962cc0ddf43813b493af6fdaa734
hnbc <184538488+hnbc03@users.noreply.github.com>
2026-03-17 16:21:58 +0700

    update navbar + hero

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,6 +7,27 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './home.html',
-  styleUrls: ['./home.css']
+  styleUrls: ['./home.css'],
 })
-export class Home {}
+export class Home implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const reveals = document.querySelectorAll('.reveal');
+
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add('active');
+            }
+          });
+        },
+        { threshold: 0.2 },
+      );
+
+      reveals.forEach((el) => observer.observe(el));
+    }
+  }
+}
