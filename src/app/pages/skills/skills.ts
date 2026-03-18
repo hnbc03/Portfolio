85429ed81c0355a651a356c435f4368b49158f4c
hnbc <184538488+hnbc03@users.noreply.github.com>
2026-03-18 23:38:39 +0700

    update content of projects and skills

--- src/app/pages/skills/skills.ts
+++ src/app/pages/skills/skills.ts
@@ -9,22 +9,22 @@ import { CommonModule } from '@angular/common';
 })
 export class Skills {
   coreSkills = [
-    { name: 'User Research', level: 85, icon: '/assets/images/icons/search.png' },
-    { name: 'Wireframing & Prototyping', level: 90, icon: 'design_services' },
-    { name: 'Usability Testing', level: 80, icon: 'verified_user' },
-    { name: 'Interaction Design', level: 75, icon: 'animation' },
-    { name: 'Design Systems', level: 70, icon: 'palette' },
-    { name: 'Human-Centered Design', level: 90, icon: 'psychology' },
+    { name: 'UX Research', level: 85, icon: '/assets/images/icons/search.png', desc: 'Conducting data-driven research and user surveys to uncover pain points and define meaningful product requirements.' },
+    { name: 'Wireframe & Prototype', level: 90, icon: 'design_services', desc: 'Transforming ideas into low to high-fidelity interactive prototypes using Figma.' },
+    { name: 'UI & Interaction Design', level: 90, icon: 'verified_user', desc: 'Designing intuitive, user-centered interfaces with a focus on micro-interactions, visual hierarchy and consistent design systems.' },
+    { name: 'Responsive Web Development', level: 85, icon: 'animation', desc: 'Building modern, mobile-first interfaces using HTML5, CSS3, and JavaScript that adapt seamlessly across devices.' },
+    { name: 'Frontend Frameworks (Angular/React)', level: 80, icon: 'palette', desc: 'Developing component-based UIs with modern frameworks and UI libraries' },
+    { name: 'Component-Driven Design', level: 80, icon: 'psychology', desc: 'Leveraging UI libraries like NgZorro and Material UI to build scalable and consistent design systems.' },
   ];
 
   tools = [
-    { name: 'Figma', icon: '/assets/images/icons/search.png', description: 'hhhígfeiuwqefkjsahfdiuhewqifhaskjfhwqiufehkjdsahfiwef', bg: '#FDECEC' }, // bạn có thể dùng SVG hoặc emoji/icon font
-    { name: 'Tailwind CSS', icon: 'tailwind-logo', description: 'hhhaefgqJDBKAJHRIUWQEHFKJahfrihfekjahefiuewhfkjhiuwehfkcjaefiwuehfciuwqehfiuwehfiuewncbkjdsahfiuwefkbcjsq', bg: '#FDECEC' },
-    { name: 'Angular', icon: 'angular-logo', description: 'hhh', bg: '#E6F7F5' },
-    { name: 'HTML / CSS / JS', icon: 'code', description: 'hhh', bg: '#FDEAEA' },
-    { name: 'FigJam / Miro', icon: 'group_work', description: 'hhh', bg: '#FDEAEA' },
-    { name: 'Notion', icon: 'note', description: 'hhh', bg: '#FDEAEA' },
-    { name: 'Basic Framer Motion', icon: 'motion_photos_on', description: 'hhh', bg: '#FDEAEA' },
+    { name: 'Figma', icon: '/assets/images/icons/search.png', description: 'hhhígfeiuwqefkjsahfdiuhewqifhaskjfhwqiufehkjdsahfiwef'}, // bạn có thể dùng SVG hoặc emoji/icon font
+    { name: 'VS Code', icon: 'tailwind-logo', description: 'hhhaefgqJDBKAJHRIUWQEHFKJahfrihfekjahefiuewhfkjhiuwehfkcjaefiwuehfciuwqehfiuwehfiuewncbkjdsahfiuwefkbcjsq'},
+    { name: 'GitHub', icon: 'angular-logo', description: 'hhh'},
+    { name: 'Google Forms', icon: 'code', description: 'hhh'},
+    { name: 'FigJam', icon: 'group_work', description: 'hhh'},
+    { name: 'Trello', icon: 'note', description: 'hhh'},
+    { name: 'Google Sheets', icon: 'motion_photos_on', description: 'hhh'},
   ];
 
   softSkills = [
@@ -42,6 +42,10 @@ export class Skills {
     },
     {
       name: 'Adaptability',
+      description: 'Nhanh chóng học và thích nghi với công cụ mới',
+    },
+    {
+      name: 'Pixel-Perfect Attention',
       description: 'Nhanh chóng học và thích nghi với công cụ mới',
     },
   ];
